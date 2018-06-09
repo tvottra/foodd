4394298a5e7206f676ed317949c2864b4b45ca5b
tvottra <tvottra@gmail.com>
2018-06-09 13:47:13 -0700

    Revert "Merge pull request #1 from tvottra/edward"
    This reverts commit 898a3143c0bf9cf6ece89d2d04410fa37b152815, reversing
    changes made to 0d22f83693c12e4cdd1b7f91c75a637cfc8d7be5.

---
+++ README.md
@@ -0,0 +1,6 @@
+# foodd
+Project for HarkerHacks 2018
+
+By Tommy V. Tran, Edward Frazer, Abhigya Sodani, Nidhir Guggilla, and Henry Liu.
+
+Plan here: https://docs.google.com/document/d/1GW-S3lkEumnwBY1RB28PZGr0r58zqYMG1-3kqt0inhU/edit
--- app/src/main/java/com/leviticus/foodd/MainActivity.java
+++ app/src/main/java/com/leviticus/foodd/MainActivity.java
@@ -3,8 +3,7 @@ package com.leviticus.foodd;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
